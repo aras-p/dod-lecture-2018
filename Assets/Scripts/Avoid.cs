bd04bf432f1d8a5c6e02081cb7db2ef1c544c46b
Aras Pranckevicius <aras@unity3d.com>
2018-09-22 22:44:46 +0300

    Avoidance system instead of individual avoids: - 100,20: 0.34ms -> 0.22ms update - 1000,20: 2.48ms -> 2.36ms update - 10000,20: 24.24ms -> 22.51ms update

--- Assets/Scripts/Avoid.cs
+++ Assets/Scripts/Avoid.cs
@@ -2,64 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// "avoid" objects with certain tag
+// "avoid" objects with certain tag. Does not do anything by itself,
+// just adds to the AvoidSystem singleton.
 public class Avoid : MonoBehaviour
 {
 	public string m_AvoidTag = "Enemy";
 	public float m_AvoidDistance = 1.0f;
 
-	private GameObject[] m_AvoidList;
-	private Transform[] m_AvoidTransforms;
-	private Color[] m_AvoidColors;
-
-	private Transform m_Transform;
-	private Move m_Mover;
-	private SpriteRenderer m_SpriteRenderer;
-
 	void Start ()
 	{
-		// find list of things to avoid; cache their transforms and colors
-		m_AvoidList = GameObject.FindGameObjectsWithTag(m_AvoidTag);
-		m_AvoidTransforms = new Transform[m_AvoidList.Length];
-		m_AvoidColors = new Color[m_AvoidList.Length];
-		for (var i = 0; i < m_AvoidList.Length; ++i)
-		{
-			m_AvoidTransforms[i] = m_AvoidList[i].transform;
-			//@TODO: this assumes the color of a sprite renderer
-			// on the things we're avoiding never changes!
-			m_AvoidColors[i] = m_AvoidList[i].GetComponent<SpriteRenderer>().color;
-		}
-
-		// cache our own component references
-		m_Transform = transform;
-		m_Mover = GetComponent<Move>();
-		m_SpriteRenderer = GetComponent<SpriteRenderer>();
-	}
-
-	void Update ()
-	{
-		if (m_AvoidList == null)
-			return;
-
-		// we'll only do anything if we have a "Mover" component, so early out if we don't
-		if (m_Mover == null)
-			return;
-
-		// check each thing in the list
-		var mypos = m_Transform.position;
-		for (var i = 0; i < m_AvoidList.Length; ++i)
-		{
-			var avoidpos = m_AvoidTransforms[i].position;
-			// is our position closer to "thing to avoid" position than the avoid distance?
-			if ((mypos-avoidpos).sqrMagnitude < m_AvoidDistance * m_AvoidDistance)
-			{
-				// tell the Move component to "resolve the collision"
-				m_Mover.ResolveCollision();
-
-				// also make our sprite take the color of the thing
-				// we just bumped into
-				m_SpriteRenderer.color = m_AvoidColors[i];
-			}
-		}
+		AvoidSystem.instance.AddToSystem(m_AvoidTag, m_AvoidDistance, gameObject);
 	}
 }
---
+++ Assets/Scripts/AvoidSystem.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// Instead of having Update() functions and individually cached "lists of things to avoid"
+// for each object, have an "avoidance system" -- all individual objects register to it,
+// the system updates all of them in one go.
+//
+// Things like "which objects to avoid" also don't need to be stored for each object now;
+// we can only store them for each unique tag & distance instead.
+public class AvoidSystem : MonoBehaviour
+{
+	public static AvoidSystem instance;
+	public void Awake()
+	{
+		instance = this;
+	}
+
+	// Each avoidance tag & distance needs to be maintained as a separate item in a dictionary.
+	class Key
+	{
+		public string tag;
+		public float distance;
+	}
+	class Value
+	{
+		// things to avoid
+		public bool initialized;
+		public Transform[] avoidTransforms;
+		public Color[] avoidColors;
+
+		// things that avoid
+		public List<Transform> transforms;
+		public List<Move> movers;
+		public List<SpriteRenderer> sprites;
+	}
+
+	Dictionary<Key, Value> m_Dictionary = new Dictionary<Key, Value>();
+
+	public void AddToSystem(string avoidTag, float avoidDistance, GameObject go)
+	{
+		Key key = new Key(){ tag = avoidTag, distance = avoidDistance };
+		AddAvoidanceKeyIfNeeded(key);
+		var v = m_Dictionary[key];
+		v.transforms.Add(go.transform);
+		v.movers.Add(go.GetComponent<Move>());
+		v.sprites.Add(go.GetComponent<SpriteRenderer>());
+	}
+
+	void AddAvoidanceKeyIfNeeded(Key key)
+	{
+		if (m_Dictionary.ContainsKey(key))
+			return;
+		var capacity = 128;
+		var val = new Value
+		{
+			initialized = false,
+			transforms = new List<Transform>(capacity),
+			movers = new List<Move>(capacity),
+			sprites = new List<SpriteRenderer>(capacity)
+		};
+		m_Dictionary.Add(key, val);
+	}
+
+	public void Update()
+	{
+		foreach(var kvp in m_Dictionary)
+		{
+			var key = kvp.Key;
+			var val = kvp.Value;
+			var avoidDistance2 = key.distance * key.distance;
+			InitializeListOfThingsToAvoidIfNeeded(key, val);
+
+			// go through all objects that are avoiding
+			for (var io = 0; io < val.transforms.Count; ++io)
+			{
+				var mypos = val.transforms[io].position;
+				// check against each thing it should be avoiding
+				for (var i = 0; i < val.avoidTransforms.Length; ++i)
+				{
+					var avoidpos = val.avoidTransforms[i].position;
+					// is our position closer to "thing to avoid" position than the avoid distance?
+					if ((mypos-avoidpos).sqrMagnitude < avoidDistance2)
+					{
+						// tell the Move component to "resolve the collision"
+						val.movers[io].ResolveCollision();
+
+						// also make our sprite take the color of the thing
+						// we just bumped into
+						val.sprites[io].color = val.avoidColors[i];
+					}
+				}
+			}
+		}
+	}
+
+	static void InitializeListOfThingsToAvoidIfNeeded(Key key, Value val)
+	{
+		if (val.initialized)
+			return;
+
+		var avoidList = GameObject.FindGameObjectsWithTag(key.tag);
+		val.avoidTransforms = new Transform[avoidList.Length];
+		val.avoidColors = new Color[avoidList.Length];
+		for (var i = 0; i < avoidList.Length; ++i)
+		{
+			val.avoidTransforms[i] = avoidList[i].transform;
+			//@TODO: this assumes the color of a sprite renderer
+			// on the things we're avoiding never changes!
+			val.avoidColors[i] = avoidList[i].GetComponent<SpriteRenderer>().color;
+		}
+
+		val.initialized = true;
+	}
+}
